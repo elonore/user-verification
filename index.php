a997b45dfd794ecd779c73142589d02f9eb2af07
Elonore <github@codestudent.anonaddy.com>
2021-03-29 15:57:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Bootstrap for styling -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Homepage</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form-div login">
+                <div class="alert alert-success">
+                    You are now logged in!
+                </div>
+
+                <h3>Welcome, Alexis</h3>
+
+                <a href="#" class="logout">logout</a>
+
+                <div class="alert alert-warning">
+                    You need to verify your account. Sign in to your email account and click on the verification link we just emailed you at <strong>contact@codestudent.anonaddy.com</strong>
+                </div>
+
+               <button class="btn btn-block btn-lg btn-primary">I am verified!</button>
+                
+
+
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
